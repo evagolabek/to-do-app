9fe1f716dab13637e0f0e74b091b698537d7136c
evagolabek <golabek.eva@gmail.com>
2018-07-15 18:42:05 +0200

    sample data

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//data model - an array with sample data(items) and different properties
+
+var todos = [
+  {
+    todoTitle: 'my first task',
+    todoResponsible: 'Eva',
+    todoDescription: 'My first task to complete',
+    todoPriority: 'high'
+  },
+  {
+    todoTitle: 'my second task',
+    todoResponsible: 'Flo',
+    todoDescription: 'My second task to complete',
+    todoPriority: 'medium'
+  },
+  {
+    todoTitle: 'my third task',
+    todoResponsible: 'Eva',
+    todoDescription: 'My third task to complete',
+    todoPriority: 'low'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
